9309414f9e1fbb987c4ce5c2cced2efe6c16749c
alexrpz <alexacuna122003@gmail.com>
2022-07-16 20:31:28 -0300

    nuevo

---
+++ formulario_login/acceso-confirmado.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso Confirmado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 py-5 bg-red">
+                <h1>Bienvenido al sistema</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-primary" role="alert">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos similique aperiam cupiditate libero autem, quas odit ut! Exercitationem, enim necessitatibus. Porro impedit facere culpa cupiditate aut doloremque adipisci dignissimos debitis!</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 d-relative">
+                <a href="index.php"><button class="btn btn-primary float-end">Salir</button></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
