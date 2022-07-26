083812dcad57423ee9364138b7d4663a8ef8aa88
alexrpz <alexacuna122003@gmail.com>
2022-07-25 23:02:17 -0300

    cambio

---
+++ calcular_iva.php
@@ -0,0 +1,94 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$iva=21;
+$precioConIva=0;
+$precioSinIva=0;
+$ivaCantidad=0;
+
+if ($_POST) {
+    $iva = $_POST["lstIva"];
+    $precioConIva = ($_POST["txtPrecioConIva"]) > 0 ? $_POST["txtPrecioConIva"] : 0;
+    $precioSinIva = ($_POST["txtPrecioSinIva"]) > 0 ? $_POST["txtPrecioSinIva"] : 0;
+    //Precio Con IVA
+    if($precioConIva > 0 && $precioSinIva = 0) {
+        $precioSinIva= $precioConIva / ($iva / 100 + 1);
+    }
+    //Precio Sin IVA
+    if($precioSinIva > 0 && $precioConIva = 0) {
+        $precioConIva= $precioSinIva * ($iva / 100 + 1);
+    }
+
+    $ivaCantidad = $precioConIva - $precioSinIva;
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de IVA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center pt-3">
+                <h1>Calculadora de IVA</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <form action="" method="POST">
+                    <div class="col-3 py-3">
+                        <label for="txtIva">IVA</label>
+                        <select name="lstIva" id="lstIva" class="form-control">
+                            <optgroup>
+                                <option value="0" selected disabled>Seleccionar</option>
+                                <option value="1">21</option>
+                                <option value="2">19.5</option>
+                                <option value="3">27</option>
+                                <option value="4">10.5</option>
+                            </optgroup>
+                        </select>
+                    </div>
+                    <div class="col-6 pb-3">
+                        <label for="txtPrecioSinIva">Precio sin IVA:</label>
+                        <input type="text" name="txtPrecioSinIva" id="txtPrecioSinIva" class="form-control">
+                    </div>
+                    <div class="col-6 pb-3">
+                        <label for="txtPrecioConIva">Precio con IVA:</label>
+                        <input type="text" name="txtPrecioConIva" id="txtPrecioConIva" class="form-control">
+                    </div>
+                    <div>
+                        <button class="btn btn-primary">CALCULAR</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-6 py-4">
+                <table class="table table:hover border">
+                    <tr>
+                        <th>IVA:</th>
+                        <td><?php echo $iva ?>%</td>
+                    </tr>
+                    <tr>
+                        <th>Precio sin IVA:</th>
+                        <td><?php echo number_format($precioSinIva, 2, ",", "."); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Precio con IVA:</th>
+                        <td><?php echo number_format($precioConIva, 2, ",", "."); ?></td>
+                    </tr>
+                    <tr>
+                        <th>IVA cantidad:</th>
+                        <td><?php echo number_format($ivaCantidad, 2, ",", "."); ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ clientes_session.php
@@ -0,0 +1,93 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$aClientes=array();
+
+if($_POST){
+    $nombre= $_POST["txtNombre"];
+    $dni= $_POST["txtDni"];
+    $telefono= $_POST["txtTelefono"];
+    $edad= $_POST["txtEdad"];
+
+    $aClientes[]=array("nombre"=> $nombre,
+                        "dni"=> $dni,
+                        "telefono"=> $telefono,
+                        "edad"=>$edad
+);
+
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de clientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Tabla de clientes</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <form action="" method="POST">
+                    <div class="pb-2">
+                        <label for="txtNombre">Nombre:</label>
+                        <input type="text" name="txtNombre" id="txtNombre" class="form-control">
+                    </div>   
+                    <div class="pb-2">
+                        <label for="txtDni">DNI:</label>
+                        <input type="text" name="txtDni" id="txtDni" class="form-control">
+                    </div> 
+                    <div class="pb-2">
+                        <label for="txtTelefono">Teléfono:</label>
+                        <input type="tel" name="txtTelefono" id="txtTelefono" class="form-control">
+                    </div> 
+                    <div class="pb-2">
+                        <label for="txtEdad">Edad:</label>
+                        <input type="number" name="txtEdad" id="txtEdad" class="form-control">
+                    </div>
+                    <div class="row">
+                        <div class="col-3">
+                            <button class="btn btn-primary" type="submit"><Em>Enviar</Em></button>
+                        </div>
+                        <div class="col-3">
+                            <button class="btn btn-danger" type="submit"><Em>Eliminar</Em></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-8">
+                <table class="table table:hover shadow border">
+                    <thead>
+                        <tr>
+                            <th>Nombre:</th>
+                            <th>DNI:</th>
+                            <th>Teléfono:</th>
+                            <th>Edad:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach($aClientes as $cliente):?>
+                            <tr>
+                                <td><?php echo $cliente["nombre"]; ?></td>
+                                <td><?php echo $cliente["dni"]; ?></td>
+                                <td><?php echo $cliente["telefono"]; ?></td>
+                                <td><?php echo $cliente["edad"]; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
